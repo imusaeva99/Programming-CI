5c4370bdf32d52394fd58796d4480df56f5a0b37
imusaeva99 <misslookbook@gmail.com>
2017-01-03 22:02:15 +0300

    ХАЙКУ

---
+++ HW/HW10/Untitled4.py
@@ -0,0 +1,454 @@
+import random
+with open('words.txt', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+
+    def nm1():
+        nm1 = list()
+        nm1 = lines[0]
+        nm1 = nm1.strip()
+        nm1 = nm1.split(' ')
+        nm1.remove(nm1[0])
+        return random.choice(nm1)
+    
+    def nf1():
+        nf1 = list()
+        nf1 = lines[1]
+        nf1 = nf1.strip()
+        nf1 = nf1.split(' ')
+        nf1.remove(nf1[0])
+        return random.choice(nf1)
+
+    def nm2():
+        nm2 = list()
+        nm2 = lines[2]
+        nm2 = nm2.strip()
+        nm2 = nm2.split(' ')
+        nm2.remove(nm2[0])
+        return random.choice(nm2)
+
+    def nf2():
+        nf2 = list()
+        nf2 = lines[3]
+        nf2 = nf2.strip()
+        nf2 = nf2.split(' ')
+        nf2.remove(nf2[0])
+        return random.choice(nf2)
+
+    def nm3():
+        nm3 = list()
+        nm3 = lines[4]
+        nm3 = nm3.strip()
+        nm3 = nm3.split(' ')
+        nm3.remove(nm3[0])
+        return random.choice(nm3)
+
+    def nf3():
+        nf3 = list()
+        nf3 = lines[5]
+        nf3 = nf3.strip()
+        nf3 = nf3.split(' ')
+        nf3.remove(nf3[0])
+        return random.choice(nf3)
+
+    def nm4():
+        nm4 = list()
+        nm4 = lines[6]
+        nm4 = nm4.strip()
+        nm4 = nm4.split(' ')
+        nm4.remove(nm4[0])
+        return random.choice(nm4)
+
+    def nf4():
+        nf4 = list()
+        nf4 = lines[7]
+        nf4 = nf4.strip()
+        nf4 = nf4.split(' ')
+        nf4.remove(nf4[0])
+        return random.choice(nf4)
+
+    def nm5():
+        nm5 = list()
+        nm5 = lines[8]
+        nm5 = nm5.strip()
+        nm5 = nm5.split(' ')
+        nm5.remove(nm5[0])
+        return random.choice(nm5)
+
+    def nf5():
+        nf5 = list()
+        nf5 = lines[9]
+        nf5 = nf5.strip()
+        nf5 = nf5.split(' ')
+        nf5.remove(nf5[0])
+        return random.choice(nf5)
+
+    def nm6():
+        nm6 = list()
+        nm6 = lines[10]
+        nm6 = nm6.strip()
+        nm6 = nm6.split(' ')
+        nm6.remove(nm6[0])
+        return random.choice(nm6)
+
+    def nf6():
+        nf6 = list()
+        nf6 = lines[11]
+        nf6 = nf6.strip()
+        nf6 = nf6.split(' ')
+        nf6.remove(nf6[0])
+        return random.choice(nf6)
+
+    def adjm1():
+        adjm1 = list()
+        adjm1 = lines[12]
+        adjm1 = adjm1.strip()
+        adjm1 = adjm1.split(' ')
+        adjm1.remove(adjm1[0])
+        return random.choice(adjm1)
+
+    def adjm2():
+        adjm2 = list()
+        adjm2 = lines[13]
+        adjm2 = adjm2.strip()
+        adjm2 = adjm2.split(' ')
+        adjm2.remove(adjm2[0])
+        return random.choice(adjm2)
+
+    def adjf2():
+        adjf2 = list()
+        adjf2 = lines[14]
+        adjf2 = adjf2.strip()
+        adjf2 = adjf2.split(' ')
+        adjf2.remove(adjf2[0])
+        return random.choice(adjf2)
+
+    def adjm3():
+        adjm3 = list()
+        adjm3 = lines[15]
+        adjm3 = adjm3.strip()
+        adjm3 = adjm3.split(' ')
+        adjm3.remove(adjm3[0])
+        return random.choice(adjm3)
+
+    def adjf3():
+        adjf3 = list()
+        adjf3 = lines[16]
+        adjf3 = adjf3.strip()
+        adjf3 = adjf3.split(' ')
+        adjf3.remove(adjf3[0])
+        return random.choice(adjf3)
+
+    def adjm4():
+        adjm4 = list()
+        adjm4 = lines[17]
+        adjm4 = adjm4.strip()
+        adjm4 = adjm4.split(' ')
+        adjm4.remove(adjm4[0])
+        return random.choice(adjm4)
+
+    def adjf4():
+        adjf4 = list()
+        adjf4 = lines[18]
+        adjf4 = adjf4.strip()
+        adjf4 = adjf4.split(' ')
+        adjf4.remove(adjf4[0])
+        return random.choice(adjf4)
+
+    def adjm5():
+        adjm5 = list()
+        adjm5 = lines[19]
+        adjm5 = adjm5.strip()
+        adjm5 = adjm5.split(' ')
+        adjm5.remove(adjm5[0])
+        return random.choice(adjm5)
+
+    def adjf5():
+        adjf5 = list()
+        adjf5 = lines[20]
+        adjf5 = adjf5.strip()
+        adjf5 = adjf5.split(' ')
+        adjf5.remove(adjf5[0])
+        return random.choice(adjf5)
+
+    def adjm6():
+        adjm6 = list()
+        adjm6 = lines[21]
+        adjm6 = adjm6.strip()
+        adjm6 = adjm6.split(' ')
+        adjm6.remove(adjm6[0])
+        return random.choice(adjm6)
+
+    def adjf6():
+        adjf6 = list()
+        adjf6 = lines[22]
+        adjf6 = adjf6.strip()
+        adjf6 = adjf6.split(' ')
+        adjf6.remove(adjf6[0])
+        return random.choice(adjf6)
+
+    def v1():
+        v1 = list()
+        v1 = lines[23]
+        v1 = v1.strip()
+        v1 = v1.split(' ')
+        v1.remove(v1[0])
+        return random.choice(v1)
+
+    def v2():
+        v2 = list()
+        v2 = lines[24]
+        v2 = v2.strip()
+        v2 = v2.split(' ')
+        v2.remove(v2[0])
+        return random.choice(v2)
+
+    def v3():
+        v3 = list()
+        v3 = lines[25]
+        v3 = v3.strip()
+        v3 = v3.split(' ')
+        v3.remove(v3[0])
+        return random.choice(v3)
+
+    def v4():
+        v4 = list()
+        v4 = lines[26]
+        v4 = v4.strip()
+        v4 = v4.split(' ')
+        v4.remove(v4[0])
+        return random.choice(v4)
+
+    def v5():
+        v5 = list()
+        v5 = lines[27]
+        v5 = v5.strip()
+        v5 = v5.split(' ')
+        v5.remove(v5[0])
+        return random.choice(v5)
+
+    def v6():
+        v6 = list()
+        v6 = lines[28]
+        v6 = v6.strip()
+        v6 = v6.split(' ')
+        v6.remove(v6[0])
+        return random.choice(v6)
+
+    def partm3():
+        partm3 = list()
+        partm3 = lines[29]
+        partm3 = partm3.strip()
+        partm3 = partm3.split(' ')
+        partm3.remove(partm3[0])
+        return random.choice(partm3)
+
+    def partm4():
+        partm4 = list()
+        partm4 = lines[30]
+        partm4 = partm4.strip()
+        partm4 = partm4.split(' ')
+        partm4.remove(partm4[0])
+        return random.choice(partm4)
+
+    def partf4():
+        partf4 = list()
+        partf4 = lines[31]
+        partf4 = partf4.strip()
+        partf4 = partf4.split(' ')
+        partf4.remove(partf4[0])
+        return random.choice(partf4)
+
+    def partm5():
+        partm5 = list()
+        partm5 = lines[32]
+        partm5 = partm5.strip()
+        partm5 = partm5.split(' ')
+        partm5.remove(partm5[0])
+        return random.choice(partm5)
+
+    def partf5():
+        partf5 = list()
+        partf5 = lines[33]
+        partf5 = partf5.strip()
+        partf5 = partf5.split(' ')
+        partf5.remove(partf5[0])
+        return random.choice(partf5)
+
+    def partm6():
+        partm6 = list()
+        partm6 = lines[34]
+        partm6 = partm6.strip()
+        partm6 = partm6.split(' ')
+        partm6.remove(partm6[0])
+        return random.choice(partm6)
+
+    def partf6():
+        partf6 = list()
+        partf6 = lines[35]
+        partf6 = partf6.strip()
+        partf6 = partf6.split(' ')
+        partf6.remove(partf6[0])
+        return random.choice(partf6)
+
+    def conj1():
+        conj1 = list()
+        conj1 = lines[36]
+        conj1 = conj1.strip()
+        conj1 = conj1.split(' ')
+        conj1.remove(conj1[0])
+        return random.choice(conj1)
+
+    def conj2():
+        conj2 = list()
+        conj2 = lines[37]
+        conj2 = conj2.strip()
+        conj2 = conj2.split(' ')
+        conj2.remove(conj2[0])
+        return random.choice(conj2)
+
+    def numm2():
+        numm2 = list()
+        numm2 = lines[38]
+        numm2 = numm2.strip()
+        numm2 = numm2.split(' ')
+        numm2.remove(numm2[0])
+        return random.choice(numm2)
+
+    def numf2():
+        numf2 = list()
+        numf2 = lines[39]
+        numf2 = numf2.strip()
+        numf2 = numf2.split(' ')
+        numf2.remove(numf2[0])
+        return random.choice(numf2)
+
+    def numm3():
+        numm3 = list()
+        numm3 = lines[40]
+        numm3 = numm3.strip()
+        numm3 = numm3.split(' ')
+        numm3.remove(numm3[0])
+        return random.choice(numm3)
+
+    def numf3():
+        numf3 = list()
+        numf3 = lines[41]
+        numf3 = numf3.strip()
+        numf3 = numf3.split(' ')
+        numf3.remove(numf3[0])
+        return random.choice(numf3)
+
+    def numm4():
+        numm4 = list()
+        numm4 = lines[42]
+        numm4 = numm4.strip()
+        numm4 = numm4.split(' ')
+        numm4.remove(numm4[0])
+        return random.choice(numm4)
+
+    def numf4():
+        numf4 = list()
+        numf4 = lines[43]
+        numf4 = numf4.strip()
+        numf4 = numf4.split(' ')
+        numf4.remove(numf4[0])
+        return random.choice(numf4)
+
+    def numm5():
+        numm5 = list()
+        numm5 = lines[44]
+        numm5 = numm5.strip()
+        numm5 = numm5.split(' ')
+        numm5.remove(numm5[0])
+        return random.choice(numm5)
+
+    def numf5():
+        numf5 = list()
+        numf5 = lines[45]
+        numf5 = numf5.strip()
+        numf5 = numf5.split(' ')
+        numf5.remove(numf5[0])
+        return random.choice(numf5)
+
+    def adv2():
+        adv2 = list()
+        adv2 = lines[46]
+        adv2 = adv2.strip()
+        adv2 = adv2.split(' ')
+        adv2.remove(adv2[0])
+        return random.choice(adv2)
+
+    def adv3():
+        adv3 = list()
+        adv3 = lines[47]
+        adv3 = adv3.strip()
+        adv3 = adv3.split(' ')
+        adv3.remove(adv3[0])
+        return random.choice(adv3)
+
+    def adv4():
+        adv4 = list()
+        adv4 = lines[48]
+        adv4 = adv4.strip()
+        adv4 = adv4.split(' ')
+        adv4.remove(adv4[0])
+        return random.choice(adv4)
+
+    def adv5():
+        adv5 = list()
+        adv5 = lines[49]
+        adv5 = adv5.strip()
+        adv5 = adv5.split(' ')
+        adv5.remove(adv5[0])
+        return random.choice(adv5)
+
+    def adv6():
+        adv6 = list()
+        adv6 = lines[50]
+        adv6 = adv6.strip()
+        adv6 = adv6.split(' ')
+        adv6.remove(adv6[0])
+        return random.choice(adv6)
+
+    def random_line_5_1():
+        sentence5_1 = [adjm4() + ' ' + nm1(), adjm3() + ' ' + nm2(), adjm2() + ' ' + nm3(), adjm4() + ' ' + nm1(),
+                       adjf4() + ' ' + nf1(), adjf3() + ' ' + nf2(), adjf2() + ' ' + nf2(), adjf4() + ' ' + nf1(),
+                       partm3() + ' ' + nm2(), partm4() + ' ' + nm1(), partf4() + ' ' + nf1(),
+                       nm5(), nf5(),
+                       numm2() + ' ' + adjm1() + ' ' + nm2(), numm2() + ' ' + adjm2() + ' ' + nm1(),
+                       numm3() + ' ' + adjm1() + ' ' + nm1(), numm4() + ' ' + nm1(),
+                       numf2() + ' ' + adjf2() + ' ' + nf1(), numf2() + ' ' + nf3(), numf3() + ' ' + nf2()]
+        return random.choice(sentence5_1)
+
+    def random_line_7_1():
+        sentence7_1 = [adjm6() + ' ' + nm1(), adjm5() + ' ' + nm2(), adjm4() + ' ' + nm3(), adjm3() + ' ' + nm4(),
+                       adjm2() + ' ' + nm5(), adjm1() + ' ' + nm6(),
+                       adjf6() + ' ' + nf1(), adjf5() + ' ' + nf2(), adjf4() + ' ' + nf3(), adjf3() + ' ' + nf4(),
+                       adjf2() + ' ' + nf5(),
+                       partm6() + ' ' + nm1(), partm5() + ' ' + nm2(), partm4() + ' ' + nm3(), partm3() + ' ' + nm4(),
+                       partf6() + ' ' + nf1(), partf5() + ' ' + nf2(), partf4() + ' ' + nf3()]
+        return random.choice(sentence7_1)
+
+    def random_line_7_2():
+        sentence7_2 = [v6() + ' ' + conj1(), adv2() + ' ' + v4() + ' ' + conj1(), adv3() + ' ' + v3() + ' ' + conj1()]
+        return random.choice(sentence7_2)
+
+    def random_line_5_2():
+        sentence5_2 = [v5(), adv2() + ' ' + v3(), adv3() + ' ' + v2(), adv4() + ' ' + v1()]
+        return random.choice(sentence5_2)
+
+    def random_line_7_3():
+        sentence7_3 = [adv2() + ' ' + v5(), adv3() + ' ' + v4(), adv4() + ' ' + v3(), adv5() + ' ' + v2(),
+                       adv6() + ' ' + v1()]
+        return random.choice(sentence7_3)
+
+    def poem():
+        p = [random_line_5_1() + '.\n' + random_line_7_1() + '.\n' + random_line_5_1(),
+             random_line_5_1() + '\n' + random_line_7_2() + '\n' + random_line_5_2(),
+             random_line_5_1() + '\n' + random_line_7_3() + '.\n' + random_line_5_1(),
+             random_line_5_1() + '\n' + random_line_7_3() + ',\n' + random_line_5_2()]
+        return random.choice(p)
+
+    print(poem()+'.')
